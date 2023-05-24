6ae867232296aa46418054d0c948bf7c708f09ae
Joel Pointon (remote) <pointon.joel@gmail.com>
2023-05-24 16:30:43 +0100

    add changes

--- code/test.py
+++ code/test.py
@@ -4,7 +4,15 @@ print(f"Is available: {torch.cuda.is_available()}")
 
 try:
     print(f"Current Devices: {torch.cuda.current_device()}")
-except :
+except:
     print('Current Devices: Torch is not compiled for GPU or No GPU')
+try:
+    print(f"Current Version: {torch.backends.cudnn.version()}")
+except:
+    print('Current Version: Unknown')
+try:
+    print(f"Cuda enabled?: {torch.backends.cudnn.enabled}")
+except:
+    print('Cuda enabled?: Unknown')
 
 print(f"No. of GPUs: {torch.cuda.device_count()}")
